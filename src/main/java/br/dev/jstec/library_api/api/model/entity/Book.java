1c2aae71423857f6ae18aa3909c795a28c325699
JS-Nascimento <jorge@jstec.dev.br>
2022-12-12 20:11:48 -0300

    finalizing the implementation in the Persistance layer in the apllication

--- src/main/java/br/dev/jstec/library_api/api/model/entity/Book.java
+++ src/main/java/br/dev/jstec/library_api/api/model/entity/Book.java
@@ -1,5 +1,12 @@
 package br.dev.jstec.library_api.api.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +16,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
 	private Integer id;
+	
+	@Column
 	private String title;
+	
+	@Column
 	private String author;
+	
+	@Column
 	private String isbn;
 }
--- src/test/java/br/dev/jstec/library_api/model/repository/BookRepositoryTest.java
+++ src/test/java/br/dev/jstec/library_api/model/repository/BookRepositoryTest.java
@@ -1,13 +1,50 @@
 package br.dev.jstec.library_api.model.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import br.dev.jstec.library_api.api.model.entity.Book;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @DataJpaTest
 public class BookRepositoryTest {
-
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	BookRepository repository;
+	
+	
+	@Test
+	@DisplayName("Must return TRUE when exist one book with the duplicated ISBN.")
+	public void returnTrueWhenISBNExist() {
+		String isbn = "123";
+		
+		Book book = Book.builder().title("As aventuras").author("fulano").isbn(isbn).build();
+		
+		entityManager.persist(book);
+		
+		boolean exists = repository.existsByIsbn(isbn);
+		
+		assertThat(exists).isTrue();
+	}
+	@Test
+	@DisplayName("Must return FALSE when don't exist book with the duplicated ISBN.")
+	public void returnFalseWhenISBNNotExist() {
+		String isbn = "123";
+					
+		boolean exists = repository.existsByIsbn(isbn);
+		
+		assertThat(exists).isFalse();
+	}
 }
